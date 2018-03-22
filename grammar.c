41a9f6f1c1eed3f86aac76009570ab0db612027d
MauvalM <mauvalm@Mau2TetV2.mint>
2018-03-22 15:54:03 +0100

    Avec Théodore

--- grammar.c
+++ grammar.c
@@ -1,1 +1,58 @@
-dDQZDQ
+#include "grammar.h"
+
+
+void createNode(struct Node *noeud, char *tagName, char *value, int lg)
+{
+  if (noeud ==NULL)
+  { // dans le cas d'un échec d'un malloc
+    exit(-1);
+
+  }
+
+  struct subNode f;
+  f = (struct subNode)malloc(sizeof(struct subNode));
+  if (f ==NULL)
+  {
+    exit(-1); // dans le cas d'un échec du malloc
+
+  }
+
+  strcpy(noeud->tag, tagName);
+  strcpy(noeud->buf, value);
+  noeud->len=lg;
+  noeud->fils = &f;
+}
+
+
+void addSubNode(struct Node *pere, struct Node *f)
+{
+  struct subNode *liste = pere->fils;
+  struct subNode *prec;
+
+  if (liste==NULL)
+  {
+    (pere->fils) = (struct subNode)malloc(sizeof(struct subNode)); // faire une vérif sur le malloc?
+    (pere->fils)->sub = f;
+    (pere->fils)->suivant = NULL;
+
+  }
+  else if (liste->sub ==NULL)
+  {
+    (pere->fils)->sub = f;
+
+  }
+  else
+  {
+    while(liste!=NULL)
+    {
+      prec = liste;
+      liste = liste->suivant;
+
+    }
+    liste  = (struct subNode)malloc(sizeof(struct subNode)); // faire une vérif sur le malloc?
+    liste->sub = f;
+    liste->suivant = NULL;
+    prec->suivant = liste;
+
+  }
+}
--- grammar.h
+++ grammar.h
@@ -1,3 +1,41 @@
-qdqzdqqdqzdsqzdqzdqzdqz
-qdzkjbqzdkjqz$qdqzdqqdqzdsqzdqzdqzdqzqd
-qldzknqzdn
+#ifndef GRAMMAR_H
+#define GRAMMAR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Ce module s'occupera de traduire des requetes et reponses HTTP tel qu'il soit
+   comprehensible par le parseur */
+
+//---------------Definitions des structures---------------//
+
+// structure des noeuds de l'arbre construit lors de la vérification
+typedef struct Node
+{
+  char *tag;  // nom de la rulename
+  char *buf;  // valeur de la rulename
+  int len;  // Longeur
+  void *fils; // pointeur sur la liste chainée des fils du noeud
+} Node;
+
+// Liste chainee de chaque noeud
+typedef struct subNode
+{
+  struct Node *sub; // Pointeur vers la structure du noeud fils
+  struct subNode *suivant; // Pointeur sur case suivante
+}subNode;
+
+
+// structure Erreur ? voir avec ZAWZ
+
+
+//--------------Prototype des fonctions----------------//
+
+void createNode(struct Node *noeud, char *tagName, char *value, int lg);
+
+void addSubNode(struct Node *pere, struct Node *f);
+
+
+
+#endif
