380870ee09c58f6a4b61ab775d8ebbaf5d657791
MauvalM <mauvalm@Mau2TetV2.mint>
2018-03-29 14:37:48 +0200

    Trucs

--- check.c
+++ check.c
@@ -1,1 +1,27 @@
 #include "check.h"
+
+
+/* Prototype des fonctions de vérification:
+
+bool checkMachin( Node* balai , char * dansLe, int cul){
+  ok = true;
+
+  1 - faire la vérfication des fils et des séparateurs grâce aux fonctions:
+    Node* new_fils;
+    if(checkFilsMachin(new_fils, là_où_on_en_est_dans_la_chaine, longueur_de_la_chaine )){
+      (balai->fils)->sub = new_fils;
+    }else{
+      ok = false;
+    }
+    ceci est à adapter en fonction du format de la rule
+
+  2 - retour:
+    return ok;
+}
+
+Ne pas oublier d'adapter ou de reprendre les modèles (voir les automates)*/
+
+bool checkHTTPMessage( Node* node, char* buf, int len)
+{
+
+}
