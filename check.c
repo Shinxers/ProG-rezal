7971bb441467159f0d1cc4680a8ce8b0850ac790
MauvalM <mauvalm@Mau2TetV2.mint>
2018-03-29 18:09:47 +0200

    bossage avec VIVI + exemple fonction de vérif

--- check.c
+++ check.c
@@ -4,8 +4,13 @@
 /* Prototype des fonctions de vérification:
 
 bool checkMachin( Node* balai , char * dansLe, int cul){
-  ok = true;
 
+  balai = pointeur vers là où on fourre les valeurs du noeud
+  dansLe = pointeur vers le premier caractère de la chaîne à vérifier
+  cul = la taille de cette chaine de caractère
+
+
+ok = true;
   1 - faire la vérfication des fils et des séparateurs grâce aux fonctions:
     Node* new_fils;
     if(checkFilsMachin(new_fils, là_où_on_en_est_dans_la_chaine, longueur_de_la_chaine )){
@@ -19,9 +24,41 @@ bool checkMachin( Node* balai , char * dansLe, int cul){
     return ok;
 }
 
-Ne pas oublier d'adapter ou de reprendre les modèles (voir les automates)*/
+Ne pas oublier d'adapter ou de reprendre les modèles (voir les automates)
+
+par exemple :
+
+bool checkToken(Node* node, char* buf, int len){
+  bool ok = true;
+  struct subNode* fils = struct
+
+  for (int i=0; i<len ; i++){
+    // struct Node* bleg = malloc(sizeof truc); pas besoin ici mais on le met pour qu'on comprenne
+    if (!checkPchar(NULL, buf+i, 1)){
+      ok=false;
+      free(bleg); // remise en liberté, car animal en voie de disparition , toi même tu sais. il est mignon,genre bien trop. EUAAAAAAAArg *mibph*
+    }else{// pas besoin non plus, mais same, etibamecus représente!
+      parcours liste chainée new_fils et ajout de bleg
+    }
+  }
+
+  if (ok==true){
+    createNode(node,"Token", buf, len);
+    return true;
+  }else{
+    node = &structerreur;
+    return false;
+  }// alé salu
+} */
+
+bool checkTchar( Node* node, char* buf, int len)
+{
+  bool ok = true;
+
+}
 
 bool checkHTTPMessage( Node* node, char* buf, int len)
 {
+  bool ok = true;
 
 }
